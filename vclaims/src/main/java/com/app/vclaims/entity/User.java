00b5488eb13938c1c9514000916e104ad5ed6e33
Manisha2605 <maneeshaprathipati@gmail.com>
2022-02-28 23:01:45 +0530

    User login changes

--- vclaims/src/main/java/com/app/vclaims/entity/User.java
+++ vclaims/src/main/java/com/app/vclaims/entity/User.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -36,7 +35,7 @@ public class User {
 		super();
 	}
 
-	public User(int userid, String firstname, String lastname, String email, long contact, char gender, String dob,
+	public User(int userid, String firstname, String lastname, String email, long contact, String gender, String dob,
 			String password) {
 		super();
 		this.userid = userid;
@@ -89,11 +88,11 @@ public class User {
 		this.contact = contact;
 	}
 
-	public char getGender() {
+	public String getGender() {
 		return gender;
 	}
 
-	public void setGender(char gender) {
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
